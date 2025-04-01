2da5d375a2444b87194c378a82a3bdf16fd989fd
Luke Cashman <lcashman@uncc.edu>
2025-04-01 17:03:01 -0400

    Add files via upload

--- UART.srcs/sources_1/new/Register_File.vhd
+++ UART.srcs/sources_1/new/Register_File.vhd
@@ -2,7 +2,7 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 04/01/2025 01:09:35 PM
+-- Create Date: 02/18/2025 04:18:50 PM
 -- Design Name: 
 -- Module Name: Register_File - Behavioral
 -- Project Name: 
@@ -20,24 +20,41 @@
 
 
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+use IEEE.std_logic_1164.all;
+use IEEE. Numeric_std. all;
 
 entity Register_File is
---  Port ( );
+    generic(
+        B: integer:=16; -- Number of bits
+        W: integer:=3 -- Number of address bits
+    );
+    port (
+        clk, reset: in std_logic; -- Clock & reset input
+        Wr_en: in std_logic; -- Permit write input
+        W_addr , r_addr : in std_logic_vector ((W-1) downto 0); -- Written & read input
+        W_data: in std_logic_vector ((B-1) downto 0); -- Written location
+        R_data: out std_logic_vector ((B-1) downto 0) -- Read location
+);
 end Register_File;
 
-architecture Behavioral of Register_File is
+architecture arch of Register_File is
 
-begin
+    type reg_file_type is array (((2**W)-1) downto 0) of std_logic_vector ((B-1) downto 0);
+    signal array_reg: reg_file_type; -- Assign array with dimensions 2^w x B
+    begin
 
-
-end Behavioral;
+    process (clk, reset)
+        begin
+        if (reset='1') then
+            array_reg <= (others=>(others=>'0')); -- Reset all rows to zero
+        elsif (rising_edge(clk)) then
+            if (wr_en='1') then -- Only write data when wr_en is high
+                array_reg(to_integer(unsigned(w_addr))) <= w_data; -- Write w_data to row w_addr
+            end if;
+        end if;
+    end process;
+    
+    --read port
+    r_data <= array_reg(to_integer(unsigned(r_addr))); -- Read r_data as row r_addr
+    
+end arch;
