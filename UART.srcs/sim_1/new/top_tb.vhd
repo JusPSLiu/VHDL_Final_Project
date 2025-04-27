e345fba5c44094121b0d3c3bb376cdf627bcfc33
Luke Cashman <lcashman@uncc.edu>
2025-04-27 09:28:18 -0400

    Add files via upload

--- UART.srcs/sim_1/new/top_tb.vhd
+++ UART.srcs/sim_1/new/top_tb.vhd
@@ -1,57 +1,73 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 04/27/2025 02:28:20 AM
--- Design Name: 
--- Module Name: top_tb - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity top_tb is
+end top_tb;
+architecture arch of top_tb is
+
+    signal WORD : integer := 8;
+    signal ADDR_W : integer := 5;
+
+-- Component Declaration
+component top is
     Generic (
         WORD : integer := 8;
         ADDR_W : integer := 5
     );
-end top_tb;
+    Port (
+        rx, clk, reset : in std_logic;
+        tx: out std_logic
+    );
+end component;
 
-architecture Behavioral of top_tb is
-    component top is
-        Generic (
-            WORD, ADDR_W : integer
-        );
-        Port (
-            rx, clk, reset : in std_logic;
-            tx: out std_logic
-        );
-    end component;
-    
-    signal rx, clk, reset, tx : std_logic;
+-- Signal Declarations
+signal rx : std_logic :='0';
+signal clk : std_logic := '0';
+signal reset : std_logic := '0';
+signal tx: std_logic;
 begin
-    system : top
-        generic map (
-            WORD => WORD,
-            ADDR_W => ADDR_W
-        )
-        port map (
-            rx => rx,
-            clk => clk,
-            reset => reset,
-            tx => tx
-        );
 
-end Behavioral;
+-- Instantiate top
+tp: top
+    port map(
+        rx => rx,
+        clk => clk,
+        reset => reset,
+        tx => tx
+    );
+    
+-- Testbench Process
+    process
+    begin
+
+        rx <= '0';
+        reset <= '1';
+        wait for 500 ns;
+        reset <= '0';
+        wait for 2000000 ns;
+        rx <= '1';
+        wait for 4000000 ns;
+        rx <= '0';
+        wait for 2000000 ns;
+        rx <= '1';
+
+    wait;
+    end process;
+
+    
+-- Clock Process
+    process
+    begin
+
+        -- Clock Cycle
+        for i in 1 to 5000000 loop -- Repeat clock cycle
+            clk <= '1';
+            wait for 100 ns;
+            clk <= '0';
+            wait for 100 ns;
+        end loop;
+
+    wait;
+    end process;
+
+end arch;
